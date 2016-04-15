48f70d5aeb836534cacebdc7ca942190ad5f8cb5
Jun Kai <kuhn96@gmail.com>
2016-04-15 21:59:05 +0800

    Created from for tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,16 @@ class Post < ActiveRecord::Base
   def posted_on
    created_at.strftime 'Posted on %B %d, %Y'
   end
+
+  # This method will extract the words and find or create a Tag
+  def str_tags=(input_tags)
+    self.tags = input_tags.split(/\W+/).map do |tag|
+      Tag.find_or_create_by(name: tag.downcase)
+    end
+  end
+
+  # This method will show the existing tags in our form
+  def str_tags
+    tags.map(&:name).join(' ')
+  end
 end
